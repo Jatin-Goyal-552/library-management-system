1222c8ad3744ebecd46d42158ed1229927e8d109
Ritik <55380296+rits008@users.noreply.github.com>
2020-09-29 23:13:50 +0530

    Update Main.java

--- project/LMS/Main.java
+++ project/LMS/Main.java
@@ -1,4 +1,3 @@
-
 package LMS;
 
 // Including Header Files.
@@ -63,7 +62,7 @@ public class Main
                 
                 Book b = books.get(input);
                 
-                if("Clerk".equals(person.getClass().getSimpleName()) || "Librarian".equals(person.getClass().getSimpleName()))
+                if("Librarian".equals(person.getClass().getSimpleName()))
                 {                
                     Borrower bor = lib.findBorrower();
 
@@ -78,7 +77,7 @@ public class Main
         //View borrower's personal information
         else if (choice == 3)
         {
-            if("Clerk".equals(person.getClass().getSimpleName()) || "Librarian".equals(person.getClass().getSimpleName()))
+            if("Librarian".equals(person.getClass().getSimpleName()))
             {
                 Borrower bor = lib.findBorrower();
                 
@@ -92,7 +91,7 @@ public class Main
         //Compute Fine of a Borrower
         else if (choice == 4)
         {
-            if("Clerk".equals(person.getClass().getSimpleName()) || "Librarian".equals(person.getClass().getSimpleName()))
+            if("Librarian".equals(person.getClass().getSimpleName()))
             {
                 Borrower bor = lib.findBorrower();
                 
@@ -241,14 +240,6 @@ public class Main
             }
         }        
             
-        //View clerk's personal information
-        else if (choice == 14)
-        {
-            Clerk clerk = lib.findClerk();
-
-            if(clerk!=null)
-                clerk.printInfo();
-        }
         
         // Functionality Performed.
         System.out.println("\nPress any key to continue..\n");
@@ -277,23 +268,24 @@ public class Main
         lib.setName("FAST Library");
         
         // Making connection with Database.
-        Connection con = lib.makeConnection();
+       
         
-        if (con == null)    // Oops can't connnect !
+       /* if (con == null)    // Oops can't connnect !
         {
-          System.out.println("\nError connecting to Database. Exiting.");
-           return;
-       }
+            System.out.println("\nError connecting to Database. Exiting.");
+            return;
+        }
+*/
         
         try {
 
-       lib.populateLibrary(con);   // Populating Library with all Records
+     //   lib.populateLibrary(con);   // Populating Library with all Records
          
         boolean stop = false;
         while(!stop)
         {   
             clrscr();
-        
+
             // FRONT END //
             System.out.println("--------------------------------------------------------");
             System.out.println("\tWelcome to Library Management System");
@@ -391,38 +383,7 @@ public class Main
                     }
                 }
                 
-                else if (person.getClass().getSimpleName().equals("Clerk"))
-                {
-                    while(true) // Way to Clerk's Portal
-                    {
-                        clrscr();
-                                        
-                        System.out.println("--------------------------------------------------------");
-                        System.out.println("\tWelcome to Clerk's Portal");
-                        System.out.println("--------------------------------------------------------");
-                        System.out.println("Following Functionalities are available: \n");
-                        System.out.println("1- Search a Book");
-                        System.out.println("2- Place a Book on hold");
-                        System.out.println("3- Check Personal Info of Borrower");
-                        System.out.println("4- Check Total Fine of Borrower");               
-                        System.out.println("5- Check Hold Requests Queue of a Book");                        
-                        System.out.println("6- Check out a Book");
-                        System.out.println("7- Check in a Book");                        
-                        System.out.println("8- Renew a Book");
-                        System.out.println("9- Add a new Borrower");
-                        System.out.println("10- Update a Borrower's Info");
-                        System.out.println("11- Logout");
-                        System.out.println("--------------------------------------------------------");                    
-                        
-                        choice = takeInput(0,12);
-
-                        if (choice == 11)
-                            break;
-                                            
-                        allFunctionalities(person,choice);                        
-                    }                    
-                }
-                
+               
                 else if (person.getClass().getSimpleName().equals("Librarian"))
                 {
                     while(true) // Way to Librarian Portal
@@ -470,7 +431,7 @@ public class Main
         }
         
         //Loading back all the records in database
-        lib.fillItBack(con);
+       
         }
         catch(Exception e)
         {
@@ -480,4 +441,3 @@ public class Main
     }    // Main Closed
     
 }   // Class closed.
-
