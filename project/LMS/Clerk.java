da89760eb5b84b866f7c531d52b349994fbb803a
JatinGoyal <56165691+Jatin-Goyal-552@users.noreply.github.com>
2020-09-29 14:52:59 +0530

    Add files via upload

---
+++ project/LMS/Clerk.java
@@ -0,0 +1,25 @@
+
+package LMS;
+
+public class Clerk extends Staff {
+  
+    int deskNo;     //Desk Number of the Clerk
+    public static int currentdeskNumber = 0;
+  
+    public Clerk(int id, String n, String a,int ph, double s,int dk) // para cons.
+    {
+        super(id,n,a,ph,s);
+        
+        if(dk == -1)
+        {
+            deskNo = currentdeskNumber;
+        }
+        else
+        {
+            deskNo=dk;
+        }
+        
+        currentdeskNumber++;
+    }
+    
+}
