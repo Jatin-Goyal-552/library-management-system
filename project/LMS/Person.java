6a3ffeafcdc512c7f913a92410c24d962de99aee
JatinGoyal <56165691+Jatin-Goyal-552@users.noreply.github.com>
2020-09-29 14:02:15 +0530

    Add files via upload

---
+++ project/LMS/Person.java
@@ -0,0 +1,80 @@
+
+package LMS;
+
+public abstract class Person 
+{   
+    protected int id;           // ID of every person related to library
+    protected String password;  // Password of every person related to library
+    protected String name;      // Name of every person related to library
+    protected String address;   // Address of every person related to library
+    protected int phoneNo;      // PhoneNo of every person related to library
+    
+    static int currentIdNumber = 0;     //This will be unique for every person, since it will be incremented when everytime
+                                       //when a person is created
+
+    public Person(int dd, String n, String a, int p)   // para cons.
+    {
+        currentIdNumber++;
+        
+        if(dd==-1)
+        {
+            id = currentIdNumber;
+        }
+        else
+            id = dd;
+        
+        password = Integer.toString(id);
+        name = n;
+        address = a;
+        phoneNo = p;
+    }        
+    
+   
+    public void setAddress(String a)
+    {
+        address = a;
+    }
+    
+    public void setPhone(int p)
+    {
+        phoneNo = p;
+    }
+    
+    public void setName(String n)
+    {
+        name = n;
+    }
+    /*----------------------------*/
+    
+    /*-------Getter FUNCs.--------*/
+    public String getName()
+    {
+        return name;
+    }
+    
+    public String getPassword()
+    {
+        return password;
+    }
+    
+     public String getAddress()
+    {
+        return address;
+    }
+     
+     public int getPhoneNumber()
+    {
+        return phoneNo;
+    }
+    public int getID()
+    {
+        return id;
+    }
+    /*---------------------------*/
+    
+     public static void setIDCount(int n)
+    {
+        currentIdNumber=n;
+    }
+   
+} // Person Class Closed
